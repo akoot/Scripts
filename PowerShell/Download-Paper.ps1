ee99bbcd968829b2e6d6511eb66e3ab62388f178
Akoot <akoot@akoot.me>
2025-03-27 15:28:24 -0400

    Fixed Update-Paper and Download-Paper
    Renamed Video-Audio to Convert-All

--- PowerShell/Download-Paper.ps1
+++ PowerShell/Download-Paper.ps1
@@ -2,15 +2,17 @@
 # Syntax: Download-Paper [version=(latest)] [output="paper-<version>.jar"] [-X]
 # -X switch allows downloading experimental versions
 
+# curl is busted on windows, fix it by running: Remove-Item alias:curl
+
 param (
-    [string]$Version=(curl -s https://api.papermc.io/v2/projects/paper | yq -r '.versions[-1]'),
+    [string]$Version=(curl -s 'https://api.papermc.io/v2/projects/paper' | yq -r '.versions[-1]'),
     [String]$Output="paper-$Version.jar",
     [switch]$X=$False
 )
 
 $Channel = If ($X) {"experimental"} Else {"default"}
-$BuildNumber = (curl -s https://api.papermc.io/v2/projects/paper/versions/$Version/builds | yq ".builds | map(select(.channel == `"$Channel`") | .build) | .[-1]")
+$BuildNumber = (curl -s "https://api.papermc.io/v2/projects/paper/versions/$Version/builds" | yq ".builds | map(select(.channel == \`"$Channel\`") | .build) | .[-1]")
 $JarName = "paper-$Version-$BuildNumber.jar"
 $Url = "https://api.papermc.io/v2/projects/paper/versions/$Version/builds/$BuildNumber/downloads/$JarName"
 
-curl -o $Output $Url
+curl -o $Output $Url 
--- PowerShell/Update-Paper.ps1
+++ PowerShell/Update-Paper.ps1
@@ -1,10 +1,17 @@
+# Update the paper in my paper repo
+# Syntax: Update-Paper [version=(latest)] [outputFile="F:\Repository\Minecraft\Paper\$Version\paper.jar"] [-X]
+# -X switch allows downloading experimental versions
+
+# curl is busted on windows, fix it by running: Remove-Item alias:curl
+
 param (
-    [string]$Version=(curl -s https://api.papermc.io/v2/projects/paper | yq -r '.versions[-1]'),
+    [string]$Version=(curl -s 'https://api.papermc.io/v2/projects/paper' | yq -r '.versions[-1]'),
     [String]$Output="F:\Repository\Minecraft\Paper\$Version\paper.jar",
     [switch]$X=$False
 )
-If(!(Test-Path -Path "F:\Repository\Minecraft\Paper\$Version")) {
-    mkdir "F:\Repository\Minecraft\Paper\$Version"
+$ParentDir = Split-Path -Path $Output -Parent
+If(!(Test-Path -Path $ParentDir)) {
+    mkdir $ParentDir
 }
 If ($X) {
     Download-Paper $Version $Output -X
